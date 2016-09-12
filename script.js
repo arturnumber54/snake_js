d182e37af4ef53aaa396175d1de4dd6cc198e366
Artur <you@example.com>
2016-09-13 00:14:44 +0400

    Создан класс Snake. Добавлена функция создания фруктов.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //
-// Создание класса
+// Создание класса Matrix
 //
 function Matrix(containerId, rows, cols) {
+	// то = это;
+	var that = this;
+
 	// id контейнера матрицы
 	this.containerId = containerId;
 
@@ -48,6 +51,103 @@ function Matrix(containerId, rows, cols) {
 		else
 			cell.className = 'cell';
 	}
+
+	// Функция добавления фрукта в случайном месте.
+	this.addFruit = function() {
+		var min = 0;
+		var max = 399;
+		var randonNum = Math.floor(Math.random() * (max - min + 1)) + min;
+		var matrix = document.getElementById(that.containerId);
+		var cell = matrix.children[randonNum];
+		cell.className = 'cell fruit';
+	}
+}
+
+//
+// Создание класса Snake.
+//
+function Snake(x, y, course) {
+	// то = это;
+	var that = this;
+
+	// Начальная координата головы змейки (x).
+	this.x = x;
+
+	// Начальная координата головы змейки (y).
+	this.y = y;
+
+	// Массив с координатами всех элементов змейки.
+	this.snake = [];
+
+	// Начальное направление движения.
+	this.course = course;
+
+	// Функция отрисовки тела змейки.
+	this.addBody = function(arr) {
+		var matrix = document.getElementById('matrix1');
+		
+		for (var j = 0; j < 400; j++) {
+			var cell = matrix.children[j];
+			var cls = cell.className;
+			var arrCls = cls.split(' '); // Создаем массив из названия класса по пробелу.
+			arrCls.forEach(function(item, i, arrCls) {
+				if (item == 'on')
+					delete arrCls[i]; // Удаляем элемент со значением 'on'.
+			});
+			cell.className = arrCls.join(' '); // Создаем строку из полученного массива.
+		}
+
+		for (var i = 0; i < arr.length; i = i + 2) {
+			var ind = (arr[i + 1] - 1) * 20 + arr[i] - 1;
+			var cell = matrix.children[ind];
+			cell.className = 'cell on';
+		}
+	}
+
+	// Функция движения змейки.
+	this.gamePlay = function() {
+		setInterval(function(){
+			if (that.course == 'right') {
+				that.x++;
+			}
+			if (that.course == 'left') {
+				that.x--;
+			}
+			if (that.course == 'up') {
+				that.y--;
+			}
+			if (that.course == 'down') {
+				that.y++;
+			}
+			that.snake.unshift(that.y);
+			that.snake.unshift(that.x);
+			that.snake.pop();
+			that.snake.pop();
+			that.addBody(that.snake);
+		}, 1000);
+	}
+
+	// Функция создания змейки.
+	this.createSnake = function(lenSnake) {
+		for (var i = 0; i < lenSnake; i++) {
+			that.snake.push(that.x - i);
+			that.snake.push(that.y);
+		}
+		that.addBody(that.snake);
+	}
+
+	// Функция обработки нажатия клавиш.
+	this.controlSnake = function() {
+		var key = event.keyCode;
+		if ((key == 37) && (that.course != 'right'))
+			that.course = 'left';
+		if ((key == 38) && (that.course != 'down'))
+			that.course = 'up';
+		if ((key == 39) && (that.course != 'left'))
+			that.course = 'right';
+		if ((key == 40) && (that.course != 'up'))
+			that.course = 'down';
+	}
 }
 
 //
@@ -60,39 +160,16 @@ window.onload = function()
 
 	// Создаем поле.
 	m1.create();	
-	// Задаем координаты игрока и мишени.
-	var pl_x = 3;
-	var pl_y = 2;
-	var target_x = 15;
-	var target_y = 16;
-	// Добавляем на поле игрока и мишень.
-	m1.setCell(pl_x, pl_y, true);
-	m1.setCell(target_x, target_y, true);
-	// Обрабатываем нажание клавиш.
-	window.onkeydown = function() {
-		var key = event.keyCode;
-		if ((pl_x == target_x) && (pl_y == target_y))
-			alert("Игра окончена, товарищ!");
-		else {
-			m1.setCell(pl_x, pl_y, false);
-			if (key == 37)
-				pl_y -= 1;
-			if (key == 38)
-				pl_x -= 1;
-			if (key == 39)
-				pl_y += 1;
-			if (key == 40)
-				pl_x += 1;
+	
+	// Создаем экземпляр класса Snake
+	var s1 = new Snake(10, 10, 'right');// По умолчанию 'right' (остальное пока не сделано)
 
-			if (pl_x > 20)
-				pl_x = 20;
-			if (pl_x < 1)
-				pl_x = 1;
-			if (pl_y > 20)
-				pl_y = 20;
-			if (pl_y < 1)
-				pl_y = 1;
-			m1.setCell(pl_x, pl_y, true);		
-		}
-	}
+	// Создаем тело змейки.
+	s1.createSnake(4);
+	s1.gamePlay();
+	window.onkeydown = s1.controlSnake;
+
+	// Создаем фрукт.
+	m1.addFruit();
+	
 }				
